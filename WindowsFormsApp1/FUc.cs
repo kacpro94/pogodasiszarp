7dc33ab07348142b67130427ff9dc93bd64d66dd
SzymonPopkiewicz <szympop@student.agh.edu.pl>
2023-12-28 15:41:47 +0100

    po szymku

---
+++ WindowsFormsApp1/FUc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FUc : UserControl
+    {
+        public FUc()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FUc_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labWeatherDescription_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labmainTemp_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/WeatherForecast.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class WeatherForecast
+    {
+        public double dt { get; set; }
+        public class weather
+        { 
+            public string main { get;set; }
+            public string description { get; set; }
+            public string icon { get; set; }
+
+        }
+        public class main
+        {
+            public double temp;
+        }
+        public class list
+        {
+            public main temp;
+            public long dt { get; set; }
+            public List<weather> weather { get; set;}
+
+        }
+        public class ForecastInfo
+        {
+            public List<list> list { get; set; }
+        }
+    }
+}
